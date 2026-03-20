52870f46a19fa715b46adf83ebcabf6aa290b793
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:45:05 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ROYAL BLOOD';
+export const nameSongs = [
+	"FIGURE IT OUT", /*1*/
+	"OUT OF THE BLACK", /*2*/
+	"LIGHTS OUT", /*3*/
+	"LITTLE MONSTER", /*4*/
+	"I ONLY LIE WHEN I LOVE YOU", /*5*/
+	"TEN TONNE SKELETON", /*6*/
+	"COME ON OVER", /*7*/
+	"HOW DID WE GET SO DARK?", /*8*/
+	"TROUBLE'S COMING", /*9*/
+	"BOILERMAKER", /*10*/
+	"TYPHOONS", /*11*/
+	"HOOK, LINE & SINKER", /*12*/
+	"BETTER STRANGERS", /*13*/
+	"LOOSE CHANGE", /*14*/
+	"HOLE", /*15*/
+	"LIMBO", /*16*/
+	"LOVE AND LEAVE IT ALONE", /*17*/
+	"BLOOD HANDS", /*18*/
+	"OBLIVION", /*19*/
+	"YOU CAN BE SO CRUEL", /*20*/
+]
